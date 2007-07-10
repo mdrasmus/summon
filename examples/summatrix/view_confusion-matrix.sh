6dcfeb7bd20aede11780cf3d3be769920f6937df
raz <raz@raz.mit.edu>
2007-07-10 00:53:10 -0400

    removed bad summatrix example

--- examples/summatrix/view_confusion-matrix.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# use summatrix to visualize a dense matrix
-#
-
-summatrix \
-    -d confusion-matrix.mat \
-    --rclab confusion-matrix.label \
-    --bgcolor 1,1,1 \
-    --style quads $*
