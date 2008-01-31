e5bf572591f8790834bf803c27d6ab681b994d89
Matt Rasmussen <rasmus@mit.edu>
2008-01-30 20:45:36 -0500

    add bootstrap to tree code

--- lib/summon/core.py
+++ lib/summon/core.py
@@ -191,7 +191,7 @@ class Element:
     def append(self, element):
         """Append a drawing element to the children of this element"""
         summon_core.append_group(self.ptr, element)
-        return aGroup
+        return element
     
     def remove(self, *elements):
         """Remove drawing elements from the children of this element"""
