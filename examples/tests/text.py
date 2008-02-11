45213b3135da0fa142f7c25028907d460615778f
raz <raz@viktor.mit.edu>
2008-02-11 09:41:11 -0500

    added text test

---
+++ examples/tests/text.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python-i
+
+
+from summon.core import *
+import summon
+from summon import shapes, colors
+
+
+win = summon.Window()
+
+win.add_group(shapes.box(0, 0, 50, 100, fill=False))
+win.add_group(text_clip("hello", 0, 0, 50, 100, 4, 20, "bottom"))
+win.add_group(group(color(1, 0, 0), text_clip2("hello", 0, 0, 50, 100, 4, 20, "bottom")))
+
+
+
+win.add_group(translate(500, 0,
+    color(1,1,1),
+    shapes.box(0, 0, 5000, 100, fill=False),
+    text_clip("hello", 0, 0, 5000, 100, 4, 20, "bottom"),
+    group(color(1, 0, 0), text_clip2("hello", 0, 0, 5000, 100, 4, 20, "bottom"))))
