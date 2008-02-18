484d3bf7e7f6249737b882443a3f4dfde4e9776c
raz <raz@viktor.mit.edu>
2008-02-18 12:40:05 -0500

    updated INSTALL file

--- make-wininst.bat
+++ make-wininst.bat
@@ -11,5 +11,5 @@ set PATH=%PATH%;C:\Python24
 rem set this to your MinGW path
 set PATH=%PATH%;C:\MinGW\bin
 
-python setup-windows.py build -cmingw32
+python setup-windows.py build --compiler=mingw32
 python setup-windows.py bdist --format=wininst
