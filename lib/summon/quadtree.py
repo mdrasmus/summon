eefefde0f1b3a7f02d88a081f7081d84bff3d263
Matt Rasmussen <rasmus@mit.edu>
2009-10-05 09:36:13 -0400

    more updates for sumtree ncbi example

--- lib/summon/quadtree.py
+++ lib/summon/quadtree.py
@@ -1,38 +1,27 @@
+"""
 
+    QuadTree data structure
 
+"""
 
-#=============================================================================
-# QuadTree data structure
-    
-class Rect:
-    """A representation of a rectangle"""       
-    
-    def __init__(self, x1, y1, x2, y2):
-        if x1 < x2:
-            self.x1 = x1
-            self.x2 = x2
-        else:
-            self.x1 = x2
-            self.x2 = x1
-        if y1 < y2:
-            self.y1 = y1
-            self.y2 = y2
-        else:
-            self.y1 = y2
-            self.y2 = y1
+def normalize_rect(rect):
+    x1, y1, x2, y2 = rect
+    if x1 > x2:
+        x1, x2 = x2, x1
+    if y1 > y2:
+        y1, y2 = y2, y1
+    return (x1, y1, x2, y2)
+        
 
-class QuadNode:
-    item = None
-    rect = None
-    
+class QuadNode:    
     def __init__(self, item, rect):
         self.item = item
         self.rect = rect
         
-        
+       
 class QuadTree:
     MAX = 10
-    MAX_DEPTH = 10
+    MAX_DEPTH = 20
     
     def __init__(self, x, y, size, depth = 0):
         self.nodes = []
@@ -42,25 +31,30 @@ class QuadTree:
         self.depth = depth
     
     def insert(self, item, rect):
+        rect = normalize_rect(rect)
+
         if len(self.children) == 0:
-            self.nodes.append(QuadNode(item, rect))
+            node = QuadNode(item, rect)
+            self.nodes.append(node)
             
             if len(self.nodes) > self.MAX and self.depth < self.MAX_DEPTH:
                 self.split()
+
+            return node
         else:
-            self.insert_into_children(item, rect)
+            return self.insert_into_children(item, rect)
     
     def insert_into_children(self, item, rect):
-        if rect.x1 < self.center[0]:
-            if rect.y1 < self.center[1]:
-                self.children[0].insert(item, rect)
-            if rect.y2 > self.center[1]:
-                self.children[1].insert(item, rect)
-        if rect.x2 > self.center[0]:
-            if rect.y1 < self.center[1]:
-                self.children[2].insert(item, rect)
-            if rect.y2 > self.center[1]:
-                self.children[3].insert(item, rect)
+        if rect[0] < self.center[0]:
+            if rect[1] < self.center[1]:
+                return self.children[0].insert(item, rect)
+            if rect[3] > self.center[1]:
+                return self.children[1].insert(item, rect)
+        if rect[2] > self.center[0]:
+            if rect[1] < self.center[1]:
+                return self.children[2].insert(item, rect)
+            if rect[3] > self.center[1]:
+                return self.children[3].insert(item, rect)
                    
     def split(self):
         self.children = [QuadTree(self.center[0] - self.size/2,
@@ -80,29 +74,31 @@ class QuadTree:
             self.insert_into_children(node.item, node.rect)
         self.nodes = []
 
-    def query(self, rect, results = {}, ret = True):
-        if ret:
-            results = {}
-        
+    def query(self, rect, results=None):
+
+        if results is None:
+            rect = normalize_rect(rect)
+            results = set()
+
         if len(self.children) > 0:
-            if rect.x1 < self.center[0]:
-                if rect.y1 < self.center[1]:
-                    self.children[0].query(rect, results, False)
-                if rect.y2 > self.center[1]:
-                    self.children[1].query(rect, results, False)
-            if rect.x2 > self.center[0]:
-                if rect.y1 < self.center[1]:
-                    self.children[2].query(rect, results, False)
-                if rect.y2 > self.center[1]:
-                    self.children[3].query(rect, results, False)
+            if rect[0] < self.center[0]:
+                if rect[1] < self.center[1]:
+                    self.children[0].query(rect, results)
+                if rect[3] > self.center[1]:
+                    self.children[1].query(rect, results)
+            if rect[2] > self.center[0]:
+                if rect[1] < self.center[1]:
+                    self.children[2].query(rect, results)
+                if rect[3] > self.center[1]:
+                    self.children[3].query(rect, results)
         else:
             for node in self.nodes:
-                if node.rect.x2 > rect.x1 and node.rect.x1 < rect.x2 and \
-                   node.rect.y2 > rect.y1 and node.rect.y1 < rect.y2:
-                    results[node.item] = True
+                if node.rect[2] > rect[0] and node.rect[0] < rect[2] and \
+                   node.rect[3] > rect[1] and node.rect[1] < rect[3]:
+                    results.add(node.item)
                     
-        if ret:
-            return results.keys()
+        return results
+
                     
     def get_size(self):
         size = 0
