5ca9dcb7cf032ebc72b12a918a2d2e8ec156aa3f
Matt Rasmussen <rasmus@mit.edu>
2010-05-20 16:37:26 -0400

    fixed bmp screenshot

--- src/Summon/Summon.cpp
+++ src/Summon/Summon.cpp
@@ -651,15 +651,15 @@ public:
         // create SDL surface
         Uint32 rmask, gmask, bmask, amask;
 #       if SDL_BYTEORDER == SDL_BIG_ENDIAN
-           rmask = 0xff000000;
-           gmask = 0x00ff0000;
-           bmask = 0x0000ff00;
-           amask = 0x000000ff;
+            rmask = 0xff000000;
+            gmask = 0x00ff0000;
+            bmask = 0x0000ff00;
+            amask = 0x000000ff;
 #       else
-           rmask = 0x000000ff;
-           gmask = 0x0000ff00;
-           bmask = 0x00ff0000;
-           amask = 0xff000000;
+            rmask = 0x000000ff;
+            gmask = 0x0000ff00;
+            bmask = 0x00ff0000;
+            amask = 0xff000000;
 #       endif
         
         static const int bpp = 32;
@@ -684,13 +684,13 @@ public:
              y >= 0 && y2 < winsize.y; 
              --y, ++y2)
         {
-	        for (int x = surface->w - 1; x >= 0; --x)
-	        {
+            for (int x = surface->w - 1; x >= 0; --x) {
                 int p = (y2 * winsize.x + x) * 3;
-		        pdata[y * surface->pitch/4 + x] = 
-			        SDL_MapRGBA(surface->format,
-				        pixels[p + 0], pixels[p + 1], pixels[p + 2], 255);
-	        }
+                pdata[y * winsize.x + x] = 
+                    SDL_MapRGBA(surface->format,
+                                pixels[p + 0], pixels[p + 1], 
+                                pixels[p + 2], 255);
+            }
         }
 
         SDL_UnlockSurface(surface);
